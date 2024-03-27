a28f9c1e613bcbdde1da6683dc408071b838801d
Bjarke Hastrup <bjarke.hastrup@gmail.com>
2024-03-27 22:48:16 +0100

    explain issue in app

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import streamlit as st
 with st.echo():
     st.write(f"sys.version: {sys.version}")
     st.write(f"sys.executable: {sys.executable}")
-    
-    
-    
-    # Should have been something like '/home/adminuser/.conda/bin/python'
+
+st.write("Should have been something like '/home/adminuser/.conda/bin/python' ")
 
