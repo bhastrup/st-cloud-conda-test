a9e9801a98e469bd7d61457d0856f1e11c2e875a
Bjarke Hastrup <bjarke.hastrup@gmail.com>
2024-03-27 22:40:59 +0100

    indentation typo

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ import streamlit as st
 with st.echo():
     st.write(f"sys.version: {sys.version}")
     st.write(f"sys.executable: {sys.executable}")
+    
+    
+    
+    # Should have been something like '/home/adminuser/.conda/bin/python'
 
